bd8bd8f23fd2c5905b8afce96324396aa51215c7
m7kang <m7kang@ucsd.edu>
2024-10-23 18:34:51 -0700

    upload

---
+++ hardware/wk3/prob3/verilog/mac.v
@@ -0,0 +1,10 @@
+// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
+// Please do not spread this code without permission 
+module mac (out, a, b, c);
+
+parameter bw = 4;
+parameter psum_bw = 16;
+
+...
+
+endmodule
