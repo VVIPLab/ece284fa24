02ee2a418a37b2db4af1fce9b8292183132acee9
m7kang <m7kang@ucsd.edu>
2024-11-07 11:54:15 -0800

    upload

---
+++ hardware/wk5/hw1_2/verilog/mac_tile.v
@@ -0,0 +1,30 @@
+// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
+// Please do not spread this code without permission 
+module mac_tile (clk, out_s, in_w, out_e, in_n, inst_w, inst_e, reset);
+
+parameter bw = 4;
+parameter psum_bw = 16;
+
+output [psum_bw-1:0] out_s;
+input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+output [bw-1:0] out_e; 
+input  [1:0] inst_w;
+output [1:0] inst_e;
+input  [psum_bw-1:0] in_n;
+input  clk;
+input  reset;
+
+...
+...
+
+mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
+        .a(a_q), 
+        .b(b_q),
+        .c(c_q),
+	.out(mac_out)
+); 
+
+...
+...
+
+endmodule
